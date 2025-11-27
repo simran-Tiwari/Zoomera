715a0b6a261d1d23630c82765ab4078a5ac2eeae
Simran- Tiwari <tiwarisimran967@gmai.com>
2025-11-27 06:37:18 +0530

    backend url added

--- FRONTEND/src/environment.js
+++ FRONTEND/src/environment.js
@@ -1,6 +1,5 @@
-//let IS_PROD = true;
-//const server = IS_PROD ?  "http://localhost:8000" : "https://zoomerabackend.onrender.com"
-
-const server = "http://localhost:8000" 
+let IS_PROD = true;
+const server = IS_PROD ?  "https://zoomera.onrender.com": "http://localhost:8000"
+//const server = "http://localhost:8000" 
 
 export default server;
